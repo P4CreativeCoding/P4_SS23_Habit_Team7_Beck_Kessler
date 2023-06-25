4daeda6cc0e271aaad3910d35c94e3033133c527
Batmania <ikessler116@gmail.com>
2023-06-25 13:25:01 +0200

    Added PulsScreen (no code)
    Added a .js file and commented what needs to be done

---
+++ med-buddy/src/Screens/PulsScreen.js
@@ -0,0 +1,6 @@
+// Hier wird der Puls angezeigt
+
+/**
+ * Hier muss auf jeden fall die verbindung zum ESP8266 stattfinden
+ * Über wlan können wir die daten von dem ESP8266 hier aufrufen und visuell anzeigen lassen
+ */
